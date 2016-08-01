065fe08b91c9b2133cf6c685d24c31cfba5e09f7
Brian W. Goldman <brianwgoldman@acm.org>
2016-08-01 11:39:37 -0600

    Tried to fix bit interpretations. Not sure if its working yet

--- src/DeepNK.cpp
+++ src/DeepNK.cpp
@@ -28,7 +28,7 @@ void DeepNK::find_optimum_nk() {
   const size_t dependencies = 2 * K;
   assert(N >= dependencies);
   const size_t patterns = 1 << dependencies;
-  const size_t function_mask = (1 << (K + 1)) - 1;
+  const size_t K_plus_1_bits = (1 << (K + 1)) - 1;
   // These are used to build and store the functions created
   // by removing a bit.
   vector<float> previous_function(patterns, 0);
@@ -37,48 +37,49 @@ void DeepNK::find_optimum_nk() {
   vector<vector<char>> best_bit_choice(N, vector<char>(patterns));
   const char TIE = 2;
   // Creates a function to mimic all NK functions that wrap
-  for (size_t f = N - K; f < N; f++) {
+  for (size_t i = 0; i < K; i++) {
     // Used to strip off bits not used by this function
-    const size_t shift_size = N - f - 1;
+    const size_t f = N - K + i;
     for (size_t pattern = 0; pattern < patterns; pattern++) {
       // Leaves only the k+1 bits used by this function
-      size_t relevant_bits = (pattern >> shift_size) & function_mask;
+      size_t relevant_bits = (pattern >> i) & K_plus_1_bits;
       previous_function[pattern] += nk_table.value(f, relevant_bits);
     }
   }
   const size_t powk = 1 << K;
-  const size_t created_function_mask = (1 << dependencies) - 1;
+  const size_t two_K_bits = (1 << dependencies) - 1;
   // "n" is the bit position we are currently trying to remove.
   // You could also say at the end of this loop the problem size will be n.
   for (size_t n = N - 1; n >= dependencies; n--) {
-    for (size_t left = 0; left < powk; left++) {
-      for (size_t right = 0; right < powk; right++) {
+    for (size_t before_wrap = 0; before_wrap < powk; before_wrap++) {
+      for (size_t after_wrap = 0; after_wrap < powk; after_wrap++) {
         // quality of setting bit "n" to zero and one.
         float zero_quality = 0, one_quality = 0;
         // The pattern the dependent bits take
-        const size_t zero_pattern = (left << (K + 1)) | right;
+        const size_t zero_pattern = before_wrap | (after_wrap << (K + 1));
         const size_t one_pattern = zero_pattern | powk;
         // bit "n" only depends on two functions at this point:
-        // NK function_{n-k} and the "previous_function"
-        zero_quality += nk_table.value(n - K, zero_pattern >> K);
-        one_quality += nk_table.value(n - K, one_pattern >> K);
-        zero_quality += previous_function[zero_pattern & created_function_mask];
-        one_quality += previous_function[one_pattern & created_function_mask];
+        // NK function_{n-k} which uses the first K+1 bits and the "previous_function"
+        zero_quality += nk_table.value(n - K, zero_pattern & K_plus_1_bits);
+        one_quality += nk_table.value(n - K, one_pattern & K_plus_1_bits);
+        // previous_function doesn't use the lowest index bit
+        zero_quality += previous_function[zero_pattern >> 1];
+        one_quality += previous_function[one_pattern >> 1];
         // put the results into "next_function"
-        const size_t hammer_pattern = (left << K) | right;
+        const size_t next_function_entry = before_wrap | (after_wrap << K);
         if (zero_quality > one_quality) {
           // Zero was better
-          next_function[hammer_pattern] = zero_quality;
-          best_bit_choice[n][hammer_pattern] = 0;
+          next_function[next_function_entry] = zero_quality;
+          best_bit_choice[n][next_function_entry] = 0;
         } else if (zero_quality == one_quality) {
           // tie
-          next_function[hammer_pattern] = zero_quality;
-          best_bit_choice[n][hammer_pattern] = TIE;
+          next_function[next_function_entry] = zero_quality;
+          best_bit_choice[n][next_function_entry] = TIE;
           std::cout << "A tie happened" << std::endl;
         } else {
           // One was better
-          next_function[hammer_pattern] = one_quality;
-          best_bit_choice[n][hammer_pattern] = 1;
+          next_function[next_function_entry] = one_quality;
+          best_bit_choice[n][next_function_entry] = 1;
         }
       }
     }
@@ -87,18 +88,17 @@ void DeepNK::find_optimum_nk() {
 
   // previous_function is now wide enough to score all remaining patterns
   // so we can just copy in the remaining NK functions
-  const size_t k_bits = (1 << K) - 1;
+
   for (size_t f = 0; f < K; f++) {
     // Used to strip off bits not used by this function
-    size_t shift_size = K - f - 1;
-    for (size_t pattern = 0; pattern < patterns; pattern++) {
-      // Leaves only the k+1 bits used by this function
-      size_t relevant_bits = (pattern >> shift_size) & function_mask;
-      // TODO Simplify this, perhaps with 2 loops
-      // The handedness of this flips
-      size_t previous_function_pattern = ((pattern >> K) & k_bits) |
-          ((pattern & k_bits) << K);
-      previous_function[previous_function_pattern] += nk_table.value(f, relevant_bits);
+    //size_t shift_size = K - f - 1;
+    for (size_t before_wrap = 0; before_wrap < powk; before_wrap++) {
+      for (size_t after_wrap = 0; after_wrap < powk; after_wrap++) {
+        const size_t previous_function_pattern = before_wrap | (after_wrap << K);
+        const size_t in_order = after_wrap | (before_wrap << K);
+        const size_t in_use = (in_order >> f) & K_plus_1_bits;
+        previous_function[previous_function_pattern] += nk_table.value(f, in_use);
+      }
     }
   }
 
@@ -113,23 +113,24 @@ void DeepNK::find_optimum_nk() {
   }
   // Extract a global optimum
   selected_inputs.resize(N);
-  size_t left = best_pattern >> K;
-  size_t right = best_pattern & k_bits;
-  // First "dependencies" bits comes from "best_pattern", but flipped
-  const size_t dependency_pattern = right << K | left;
+  const size_t k_bits = (1 << K) - 1;
+  size_t after_wrap = best_pattern >> K;
+  size_t before_wrap = best_pattern & k_bits;
+  // First "dependencies" bits comes from "best_pattern"
+  const size_t dependency_pattern = after_wrap | (before_wrap << K);
   for (size_t i = 0; i < dependencies; i++) {
-    selected_inputs[i] = (dependency_pattern >> (dependencies - i - 1)) & 1;
+    selected_inputs[i] = (dependency_pattern >> i) & 1;
   }
   // Use the previous "left" and "right" to recover the way to set bit i
   for (size_t i = dependencies; i < N; i++) {
-    auto best_bit = best_bit_choice[i][left << K | right];
+    auto best_bit = best_bit_choice[i][before_wrap | (after_wrap << K)];
     if (best_bit == TIE) {
       // TODO Handle this better
       best_bit = 0;
     }
     selected_inputs[i] = best_bit;
     // shift out old bit information, add in new bit
-    left = ((left << 1) & k_bits) | best_bit;
+    before_wrap = (before_wrap >> 1) | (best_bit << (K-1));
   }
   estimated_quality = previous_function[best_pattern];
 }
